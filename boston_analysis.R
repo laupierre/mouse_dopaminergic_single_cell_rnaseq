4f5595bacf239726cbdd220fb77fc849cca9b9ca
laupierre <laupierre@users.noreply.github.com>
2023-06-13 10:22:44 +0200

    Update boston_analysis.R

--- boston_analysis.R
+++ boston_analysis.R
@@ -4,8 +4,20 @@ library (Seurat)
 
 neurons <- readRDS ("neurons.rds")
 
-# update the old seurat object
-neurons <- UpdateSeuratObject(object = neurons)
+# update the old seurat 2 object from the raw.data slot
+neurons5 <- CreateSeuratObject(counts = neurons@raw.data, project = "dopaminergic", min.cells = 3, min.features = 200)
+neurons5@meta.data <- neurons@meta.data
+neurons <- neurons5
 
 head(neurons@meta.data, n=5)
 table (neurons@meta.data$Curated_cellTypeLabels)
+
+Idents (neurons) <- "Curated_cellTypeLabels"
+length (WhichCells(neurons, idents = "Dopaminergic Neurons"))
+#444
+
+neurons <-  subset(neurons, subset = Th > 1)
+# 389
+
+# Normalize the data
+neurons <- NormalizeData(neurons, normalization.method = "LogNormalize", scale.factor = 10000)
