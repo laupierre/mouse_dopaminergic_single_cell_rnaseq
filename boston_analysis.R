515b4945a73f3fbc9b373b216aceb1fb65831dd1
laupierre <laupierre@users.noreply.github.com>
2023-06-13 10:44:54 +0200

    Update boston_analysis.R

--- boston_analysis.R
+++ boston_analysis.R
@@ -16,8 +16,44 @@ Idents (neurons) <- "Curated_cellTypeLabels"
 length (WhichCells(neurons, idents = "Dopaminergic Neurons"))
 #444
 
-neurons <-  subset(neurons, subset = Th > 1)
+neurons <-  subset(neurons, subset = Th > 1 & percent.mito < 10)
 # 389
+
 
 # Normalize the data
 neurons <- NormalizeData(neurons, normalization.method = "LogNormalize", scale.factor = 10000)
+
+# Identification of 2000 highly variable features 
+neurons <- FindVariableFeatures(neurons, selection.method = "vst", nfeatures = 2000)
+
+# Get the 10 most highly variable genes
+top10 <- head(VariableFeatures(neurons), 10)
+
+# plot the variable features with and without labels
+plot1 <- VariableFeaturePlot(neurons)
+plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+plot2
+
+# Scaling the data on the 2000 higlhy variable features
+neurons <- ScaleData (neurons)
+
+# perform PCA on the scaled data
+neurons <- RunPCA (neurons, features = VariableFeatures(object = neurons))
+
+VizDimLoadings(neurons, dims = 1:2, reduction = "pca")
+
+# See how many PCA dimensions are visible (7 are possibly visible)
+DimHeatmap (neurons, dims = 1:10, cells = 350, balanced = TRUE)
+ElbowPlot(neurons)
+# 10 dimensions is flat already
+
+# Cluster the cells
+neurons <- FindNeighbors(neurons, dims = 1:10)
+neurons <- FindClusters(neurons, resolution = 0.5)
+
+
+
+
+
+
+
